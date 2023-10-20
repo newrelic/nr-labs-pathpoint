f170d4d5386597f0750fe3dbcdad16bddde196ee
shahram <shahram@newrelic.com>
2023-10-20 00:17:22 -0700

    refactor: add 'selected' attribute to signals record

--- src/utils/stage.js
+++ src/utils/stage.js
@@ -20,6 +20,7 @@ export const addSignalStatuses = (stages = [], serviceLevelsData = {}) =>
             target,
             nrql,
             status: signalStatus({ type, attainment, target }),
+            selected: false,
           };
         }),
       })),
