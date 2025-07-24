7133623b7b1a66032569a19684466bbe2d6643e5
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2025-07-23 17:30:30 -0700

    style: prettier fix

--- src/constants/statuses.js
+++ src/constants/statuses.js
@@ -2,12 +2,6 @@ import { StatusIcon } from '@newrelic/nr-labs-components';
 
 export const { STATUSES } = StatusIcon;
 
-export const UNHEALTHY_STATUSES = [
-  STATUSES.CRITICAL,
-  STATUSES.WARNING,
-];
+export const UNHEALTHY_STATUSES = [STATUSES.CRITICAL, STATUSES.WARNING];
 
-export const OK_STATUSES = [
-  STATUSES.SUCCESS,
-  STATUSES.UNKNOWN,
-];
+export const OK_STATUSES = [STATUSES.SUCCESS, STATUSES.UNKNOWN];
