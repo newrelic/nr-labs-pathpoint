5e627c8a12bcc1f5dc269874b9dd02bcb2a7f628
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2025-08-17 23:29:06 -0700

    fix: no signals msg on steps with inactive signals

--- src/components/step/index.js
+++ src/components/step/index.js
@@ -303,7 +303,7 @@ const Step = ({
       />
       {mode === MODES.EDIT ? (
         <>
-          {signals.length ? (
+          {thisStep?.signals.length ? (
             <div className="add-signal-btn">
               <Button
                 className="button-tertiary-border"
